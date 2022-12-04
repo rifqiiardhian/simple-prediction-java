0274126e9c0473b18e87d8d70fcb83dd466c31f4
rifqiiardhian <rifqiardhian@gmail.com>
2022-12-04 20:08:30 +0700

    add comments

--- src/Main.java
+++ src/Main.java
@@ -4,57 +4,78 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Define input variables
         String userName, spouseName;
         int userAge, spouseAge;
 
         Scanner input = new Scanner(System.in);
 
+        // init program
         System.out.println("Selamat Datang Di Program Ramalan Cupu");
         System.out.println("++++++++++++++++++++++++++++++++++++++\n");
 
+        // Input data user
         System.out.println("Data Anda :");
         loveSymbols("user");
 
+        // Input nama user
+        // Type String
         System.out.print("Masukkan nama anda : ");
         userName = input.nextLine();
 
+        // Input umur user
+        // Type Integer
         System.out.print("Masukkan umur anda : ");
         userAge = Integer.parseInt(input.nextLine());
 
+        // Input data pasangan
         System.out.println("\nData Pasangan Anda :");
         loveSymbols("spouse");
 
+        // Input nama pasangan
+        // Type String
         System.out.print("Masukkan nama pasangan anda : ");
         spouseName = input.nextLine();
 
+        // Input umur pasangan
+        // Type Integer
         System.out.print("Masukkan umur pasangan anda : ");
         spouseAge = Integer.parseInt(input.nextLine());
 
+        // Check all data
         if (userName == null || userAge == 0 || spouseName == null || spouseAge == 0) {
+            // if data is empty
             System.out.println("Anda harus melengkapi data diatas!");
         } else {
+            // passing data to get prediction
             getPrediction(userName, userAge, spouseName, spouseAge);
         }
     }
 
+    // Method to predict cocoklogi
     private static void getPrediction(String uName, int uAge, String sName, int sAge) {
         int up, low, random;
         double cocoklogi;
 
+        // define variables for random number
         up = 100;
         low = 50;
         random = (int)(Math.random() * (up - low)) + low;
 
+        // formula to predict cocoklogi
         cocoklogi = random / 1.1;
 
+        // output data
         System.out.println("\n" +uName+ " [" +uAge+ "] tahun");
         loveSymbols("relation");
         System.out.println(sName+ " [" +sAge+ "] tahun\n");
+        // state enter to show data result
         runEnter();
         System.out.println("Kecocokan anda dengan pasangan anda adalah : " +String.format("%.2f", cocoklogi)+ "%\n");
         System.out.println("Terimakasih karena anda telah menggunakan jasa ramalan kami... :D");
     }
 
+    // Method run enter to trigger get the data result
     private static void runEnter(){
         System.out.print("Tekan ENTER untuk melihat hasil ramalan...");
 
@@ -65,6 +86,7 @@ public class Main {
         }
     }
 
+    // Method output love symbols
     private static void loveSymbols(String type) {
         switch (type) {
             case "user":
